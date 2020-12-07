c900ea4168cb4fcbeaba748d4ff46bffe1f57eae
kobe-cb <kbrag003@ucr.edu>
2020-12-07 12:07:57 -0800

    Fixed Login Functionality for Compilation

--- src/main.cpp
+++ src/main.cpp
@@ -16,15 +16,27 @@ int main(int argc, char *argv[]) {
 
    //===================LOGIN READ===================
    string userfile; // = argv[1];
-   cout << "User file?" << endl;
-   //cin.ignore();
-   cin >> userfile;
-   cout << endl;
-   string passfile;; // = argv[2];
-   cout << "Password file?" << endl;
-   //cin.ignore();
-   cin >> passfile;
-   cout << endl;
+   string passfile;
+   cout << "Load default(1) or custom credential database?(2)" << endl;
+   int userAction(0);
+   cin >> userAction;
+   if (userAction == 1) {
+      userfile = "usernames.txt";
+      passfile = "passwords.txt";
+   }
+   else if (userAction == 2) {
+      cout << "Please enter the username's filename: " << endl;
+      cin >> userfile;
+      cout << endl;
+
+      cout << "Please enter the password's filename: " << endl;
+      cin >> passfile;
+      cout << endl;
+   }
+   else {
+      cout << "Incorrect Option, ending program" << endl;
+      return 1;
+   }
 
    if (!prog.loadLogin(userfile, passfile)) {
       cout << "Error loading users from file " << userfile << endl;
@@ -33,15 +45,17 @@ int main(int argc, char *argv[]) {
 
    if (!prog.login()) {
       cout << "Login unsuccessful, now exiting the program" << endl;
+      prog.offloadLogin(userfile, passfile);
       return 1;
    }
+   
 
    //===================PROGRAM RUN===================
    prog.run();
 
 
    //===================SAVE LOGIN INFO TO FILE===================
-   //prog.offloadLogin(userfile, passfile);
+   prog.offloadLogin(userfile, passfile);
 
    return 0;
 }
